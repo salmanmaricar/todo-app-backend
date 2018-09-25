929758394c3c989d590d38288d83c877f7ce5d36
Saiful Shahril Saini <saifulss85@gmail.com>
2018-09-25 23:03:07 +0800

    Include Express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log('Running server...');
+import express = require("express");
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Express is running...');
+});
