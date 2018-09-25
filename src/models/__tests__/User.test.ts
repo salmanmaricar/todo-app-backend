9a772b65ba9aefcd93bd3056c302aaa010c4f276
Saiful Shahril Saini <saifulss85@gmail.com>
2018-09-26 00:20:48 +0800

    Add Jest and the ability to run from within IDE as well as CLI, and coverage too

---
+++ src/models/__tests__/User.test.ts
@@ -0,0 +1,21 @@
+import { User } from '../User';
+
+describe('User', () => {
+  const user = new User('John');
+
+  it('can get correct initials', () => {
+    const initials = user.getInitials();
+
+    expect(initials).toEqual('J');
+  });
+
+  it('can get name', () => {
+    expect(user.name).toEqual('John');
+  });
+
+  it('can set name', () => {
+    expect(user.name).toEqual('John');
+    user.name = 'Bob';
+    expect(user.name).toEqual('Bob');
+  });
+});
